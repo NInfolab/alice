b9409d303e6611f640175d26bf1b240b2278fc59
Angelo Delefortrie <angelo.delefortrie@gmail.com>
2019-03-09 19:06:49 +0100

    Fix redirection regexp (Bad redirection location when subdomain added)

--- modules/redirection.js
+++ modules/redirection.js
@@ -1,6 +1,8 @@
 module.exports = (proxy, serverConfig, moduleConfig = {}) => {
   // Replace origin host to use relative urls in redirections
-  const redirRgx = new RegExp(`(https?:)?(/{2})?${serverConfig.parsed_target.host}`, 'gi')
+  const redirRgx = new RegExp(`^(https?:)?(/{2})?${serverConfig.parsed_target.host}`, 'gi')
+
+  // @TODO: If domain change, display warning page before redirection
 
   proxy.on('proxyRes', (proxyRes) => {
     if (proxyRes.statusCode >= 301 && proxyRes.statusCode <= 302) {
