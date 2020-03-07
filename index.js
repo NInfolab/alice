d39f88ffa292da5f8004cd7fd7a82b7baf1e1842
Geraldine Gonzalez Lopez <geralidine@gonzal.ez>
2020-03-07 11:50:34 +0100

    Restructure index.js so it exports a 'start' function

--- index.js
+++ index.js
@@ -6,30 +6,44 @@ const http = require('http')
 const config = require('./configLoader')
 const modules = require('./modules')
 
-// Basic Connect App
-const app = connect()
+const createProxy = (config) => {
+  // Basic Connect App
+  const app = connect()
 
-// Initialize reverse proxy
-const proxy = httpProxy.createProxyServer({ secure: false })
+  // Initialize reverse proxy
+  const proxy = httpProxy.createProxyServer({ secure: false })
 
-// Apply modules
-if (config.modules && config.modules.length) {
-  config.modules.forEach((module) => {
-    if (typeof module === 'string' && modules[module]) {
-      app.use(modules[module](proxy, config))
-    } else {
-      const [moduleName, moduleConfig] = module
+  // Apply modules
+  if (config.modules && config.modules.length) {
+    config.modules.forEach((module) => {
+      if (typeof module === 'string' && modules[module]) {
+        app.use(modules[module](proxy, config))
+      } else {
+        const [moduleName, moduleConfig] = module
 
-      if (modules[moduleName]) {
-        app.use(modules[moduleName](proxy, config, moduleConfig))
+        if (modules[moduleName]) {
+          app.use(modules[moduleName](proxy, config, moduleConfig))
+        }
       }
-    }
+    })
+  }
+
+  return http.createServer(app)
+}
+
+const main = () => {
+  return createProxy(config).listen(config.port, 'localhost', () => {
+    console.log(`Server listen on http://127.0.0.1:${config.port}`)
   })
 }
 
-http.createServer(app).listen(config.port, 'localhost', () => {
-  console.log(`Server listen on http://127.0.0.1:${config.port}`)
-})
+if (require.main === module) {
+  main()
+}
+
+module.exports = {
+  createProxy
+}
 
 // Local Variables:
 // mode: js2
